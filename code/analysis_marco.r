d83911a6fbd8ce1fbcff7ce865726d24c03a2631
Wandaboma <42309408+Wandaboma@users.noreply.github.com>
2024-03-22 19:17:31 +0800

    Update analysis_marco.r

--- code/analysis_marco.r
+++ code/analysis_marco.r
@@ -1,40 +1,23 @@
-library("car")
-library("lme4")
+#on each project
+library(r2mlm)
+library(rsq)
+library(car)
 library("performance")
-library("emmeans")
-library("data.table") 
-library("dplyr")
-library("tidyverse")    # needed for data manipulation
-library("knitr")
-library("stringr")
-library("lmerTest")     # to get p-value estimations
-library("performance")
-library("car")
-library("pscl")
-library("sjstats")
-library("ggplot2")
-library("MASS")
-library("mlmRev")
-library("agridat")
-library("MCMCglmm")
-library("plotMCMC")
-library("see")
-library("patchwork")
 
-result <- read.csv('D:/Research/issue process mining/data/mean_detailed_2020.csv')
-result = result[which(result$slice_time >= '2020-01-01'),]
 projectList <- c("microsoft/vscode" ,"flutter/flutter" , "golang/go" , "dart-lang/sdk", 
                  "dotnet/runtime", "elastic/kibana" ,"rust-lang/rust" , "kubernetes/kubernetes",
                  "cockroachdb/cockroach", "tensorflow/tensorflow","microsoft/TypeScript", 
                  "godotengine/godot", "ansible/ansible", 
                  "elastic/elasticsearch", "rancher/rancher", "dotnet/roslyn" , "qgis/QGIS")
-library(r2mlm)
-library(rsq)
+
+result <- read.csv("D:/Research/issue process mining/data/mean.csv")
+result = result[which(result$slice_time >= '2020-01-01'),]
+result$delta_time <- result$resolution_time - result$before_average_time
 
 data <- data.frame()
 for (i in 1: 17) {
   issuedata = result[result$project_name == projectList[i],]
-  issuedata$transition_time <- log(issuedata$transition_time+1)
+  issuedata$resolution_time <- log(issuedata$resolution_time+1)
   issuedata$event_number <- log(issuedata$event_number+1)
   issuedata$issue_length <- log(issuedata$issue_length+1)
   issuedata$participant_number <- log(issuedata$participant_number+1)
@@ -45,9 +28,10 @@ for (i in 1: 17) {
   issuedata$core_label <- log(issuedata$core_label + 1)
   issuedata$core_subscribe <- log(issuedata$core_subscribe + 1)
   issuedata$before_average_time <- log(issuedata$before_average_time+1)
-  issuedata$before_trans_time <- log(issuedata$before_trans_time + 1)
-  
-  # issuedata$transition_time <- scale(issuedata$transition_time)
+  issuedata$slice_average_time <- log(issuedata$slice_average_time + 1)
+  # issuedata$delta_time <- log(issuedata$delta_time + 825)
+
+  # issuedata$resolution_time <- scale(issuedata$resolution_time)
   issuedata$event_number <- scale(issuedata$event_number)
   issuedata$issue_length <- scale(issuedata$issue_length)
   issuedata$participant_number <- scale(issuedata$participant_number)
@@ -58,18 +42,20 @@ for (i in 1: 17) {
   issuedata$core_label <- scale(issuedata$core_label)
   issuedata$core_subscribe <- scale(issuedata$core_subscribe)
   issuedata$before_average_time <- scale(issuedata$before_average_time)
-  issuedata$before_trans_time <- scale(issuedata$before_trans_time)
+  issuedata$slice_average_time <- scale(issuedata$slice_average_time)
+  # issuedata$delta_time <- scale(issuedata$delta_time)
   data <- rbind(data, issuedata)
 }
+# macro.model = lm(resolution_time ~ event_number + participant_number + entropy + core_issue + normal_issue +
+#                       core_assign + core_label + core_subscribe + before_average_time + entropy : event_number  + entropy : participant_number
+#                     + entropy : core_issue + entropy : normal_issue + entropy : core_assign + entropy : core_label
+#                     + entropy : core_subscribe + entropy : before_average_time, data = data)
 
-data$merge <- paste(data$event_name, data$project_name)
-trans.model = glmer(transition_time ~ event_number + participant_number + entropy + core_issue + 
-                      normal_issue + core_assign + core_label + core_subscribe + before_trans_time +
-                      entropy : event_number
-                    + entropy : participant_number + entropy : core_issue + entropy : normal_issue + entropy : core_assign + entropy : core_label
-                    + entropy : core_subscribe  + before_trans_time : entropy + (1 | merge), data = data)
+macro.model = glmer(resolution_time ~ event_number + participant_number + entropy + core_issue + normal_issue +
+                    core_assign + core_label + core_subscribe + before_average_time + entropy : event_number  + entropy : participant_number
+                  + entropy : core_issue + entropy : normal_issue + entropy : core_assign + entropy : core_label
+                  + entropy : core_subscribe + entropy : before_average_time + (1 | project_name), data = data)
 
-vif(trans.model)
-summary(trans.model)
-model_performance(trans.model)
-print(Anova(trans.model, type = "III"))
+summary(macro.model)
+model_performance(macro.model)
+print(Anova(macro.model, type = "III"))
