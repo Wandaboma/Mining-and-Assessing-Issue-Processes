1ef70ebe8f7bffbb4ed540ed99135c55a6fea0c6
Wandaboma <42309408+Wandaboma@users.noreply.github.com>
2024-03-22 19:17:53 +0800

    Update analysis_micro.r

--- code/analysis_micro.r
+++ code/analysis_micro.r
@@ -21,88 +21,55 @@ library("plotMCMC")
 library("see")
 library("patchwork")
 
-data <- read.csv("D:/FSE/mean_detailed_with_before_transtime_merged.csv")
-data = data[data$before_trans_time > 0,]
-data$transition_time <- log(data$transition_time + 1)
-data$event_number <- log(data$event_number + 1)
-data$issue_length <- log(data$issue_length + 1)
-data$participant_number <- log(data$participant_number + 1)
-data$entropy <- log(data$entropy + 1)
-data$core_issue <- log(data$core_issue + 1)
-data$normal_issue <- log(data$normal_issue + 1)
-data$core_assign <- log(data$core_assign + 1)
-data$core_label <- log(data$core_label + 1)
-data$core_subscribe <- log(data$core_subscribe + 1)
-data$before_average_time <- log(data$before_average_time + 1)
-data$before_trans_time <- log(data$before_trans_time + 1)
+result <- read.csv('D:/Research/issue process mining/data/mean_detailed_2020.csv')
+result = result[which(result$slice_time >= '2020-01-01'),]
+projectList <- c("microsoft/vscode" ,"flutter/flutter" , "golang/go" , "dart-lang/sdk", 
+                 "dotnet/runtime", "elastic/kibana" ,"rust-lang/rust" , "kubernetes/kubernetes",
+                 "cockroachdb/cockroach", "tensorflow/tensorflow","microsoft/TypeScript", 
+                 "godotengine/godot", "ansible/ansible", 
+                 "elastic/elasticsearch", "rancher/rancher", "dotnet/roslyn" , "qgis/QGIS")
+library(r2mlm)
+library(rsq)
 
-data$transition_time <- scale(data$transition_time)
-data$event_number <- scale(data$event_number)
-data$issue_length <- scale(data$issue_length)
-data$participant_number <- scale(data$participant_number)
-data$entropy <- scale(data$entropy)
-data$core_issue <- scale(data$core_issue)
-data$normal_issue <- scale(data$normal_issue)
-data$core_assign <- scale(data$core_assign)
-data$core_label <- scale(data$core_label)
-data$core_subscribe <- scale(data$core_subscribe)
-data$before_average_time <- scale(data$before_average_time)
-data$before_trans_time <- scale(data$before_trans_time)
-
-get_points <- function(x, n = 3) {
-  x_mean <- mean(x)  
-  x_sd   <- sd(x) 
+data <- data.frame()
+for (i in 1: 17) {
+  issuedata = result[result$project_name == projectList[i],]
+  issuedata$transition_time <- log(issuedata$transition_time+1)
+  issuedata$event_number <- log(issuedata$event_number+1)
+  issuedata$issue_length <- log(issuedata$issue_length+1)
+  issuedata$participant_number <- log(issuedata$participant_number+1)
+  issuedata$entropy <- log(issuedata$entropy + 1)
+  issuedata$core_issue <- log(issuedata$core_issue + 1)
+  issuedata$normal_issue <- log(issuedata$normal_issue + 1)
+  issuedata$core_assign <- log(issuedata$core_assign + 1)
+  issuedata$core_label <- log(issuedata$core_label + 1)
+  issuedata$core_subscribe <- log(issuedata$core_subscribe + 1)
+  issuedata$before_average_time <- log(issuedata$before_average_time+1)
+  issuedata$before_trans_time <- log(issuedata$before_trans_time + 1)
   
-  x_bt   <- min(x)  
-  x_m2   <- x_mean - 2 * x_sd 
-  x_m1   <- x_mean - x_sd 
-  x_ct   <- x_mean
-  x_p1   <- x_mean + x_sd 
-  x_p2   <- x_mean + 2 * x_sd
-  x_up   <- max(x) 
-  
-  x_bt_r <- round(x_bt, digits = 1)
-  x_m2_r <- round(x_m2, digits = 1)
-  x_m1_r <- round(x_m1, digits = 1)
-  x_ct_r <- round(x_ct, digits = 1)
-  x_p1_r <- round(x_p1, digits = 1)
-  x_p2_r <- round(x_p2, digits = 1)
-  x_up_r <- round(x_up, digits = 1)
-  
-  if (n == 3) {
-    points <- c(x_m1_r, x_ct_r, x_p1_r)
-  } else if (n == 5) {
-    points <- c(x_bt_r, x_m1_r, x_ct_r, x_p1_r, x_up_r)
-  } else {
-    points <- c(x_bt_r, x_m2_r, x_m1_r, x_ct_r, x_p1_r, x_p2_r, x_up_r)
-  }
-  return(points)
+  # issuedata$transition_time <- scale(issuedata$transition_time)
+  issuedata$event_number <- scale(issuedata$event_number)
+  issuedata$issue_length <- scale(issuedata$issue_length)
+  issuedata$participant_number <- scale(issuedata$participant_number)
+  issuedata$entropy <- scale(issuedata$entropy)
+  issuedata$core_issue <- scale(issuedata$core_issue)
+  issuedata$normal_issue <- scale(issuedata$normal_issue)
+  issuedata$core_assign <- scale(issuedata$core_assign)
+  issuedata$core_label <- scale(issuedata$core_label)
+  issuedata$core_subscribe <- scale(issuedata$core_subscribe)
+  issuedata$before_average_time <- scale(issuedata$before_average_time)
+  issuedata$before_trans_time <- scale(issuedata$before_trans_time)
+  data <- rbind(data, issuedata)
 }
 
-generate_and_save_emmip <- function(rq_model,formula, file,vary_project_age_fix_fork_entropy) {
-  # formula: fork_entropy_scale ~ project_age_scale
-
-  emmip_data <- emmip(rq_model, formula, at = vary_project_age_fix_fork_entropy, plotit = FALSE)
-  write.csv(as.data.table(emmip_data),
-            file <- file,
-            row.names = FALSE,
-            fileEncoding = "UTF-8")
-  
-  cat("emmip data saved to", file, "\n")
-}
-
+data$merge <- paste(data$event_name, data$project_name)
 trans.model = glmer(transition_time ~ event_number + participant_number + entropy + core_issue + 
-               normal_issue + core_assign + core_label + core_subscribe + before_trans_time + before_average_time + entropy : event_number
-               + entropy : participant_number + entropy : core_issue + entropy : normal_issue + entropy : core_assign + entropy : core_label
-               + entropy : core_subscribe + entropy : before_trans_time + entropy : before_average_time + (1 | merge), data = data)
+                      normal_issue + core_assign + core_label + core_subscribe + before_trans_time +
+                      entropy : event_number
+                    + entropy : participant_number + entropy : core_issue + entropy : normal_issue + entropy : core_assign + entropy : core_label
+                    + entropy : core_subscribe  + before_trans_time : entropy + (1 | merge), data = data)
 
 vif(trans.model)
 summary(trans.model)
 model_performance(trans.model)
 print(Anova(trans.model, type = "III"))
-
-entropy_c <- get_points(result$entropy)
-data_point <- list(
-  entropy=entropy_c,
-  before_trans_time=seq(entropy_c[[1]], entropy_c[[length(entropy_c)]], by = 0.1))
-generate_and_save_emmip(rq_model=trans.model,entropy ~ before_trans_time,file = "D:/FSE/micro_entropy_before_trans_time.csv",data_point)
