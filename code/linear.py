ccd5330fec0e7ac6f2d026078f7eeabfe105461b
Wandaboma <42309408+Wandaboma@users.noreply.github.com>
2024-03-23 16:49:47 +0800

    Delete code/linear.py

--- code/linear.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-# import utility function for sample data path
-from pymer4.utils import get_resource_path
-resultPath = "/home/sbh/APSEC/result/2023-08-03T10:56Z(0.4)/"
-
-df = pd.read_csv(resultPath +'mean_1.csv')
-print(df)
