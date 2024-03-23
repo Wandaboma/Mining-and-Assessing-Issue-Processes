0ee4522fe0f4d409ff9abb5f7b0953171258f668
Wandaboma <42309408+Wandaboma@users.noreply.github.com>
2024-03-23 16:47:28 +0800

    Delete code/linear_analysis.py

--- code/linear_analysis.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-resultPath = "/home/sbh/APSEC/result/2023-09-13T16:31Z/"
-
-df = pd.read_csv(resultPath +'mean_detailed_1.csv')
-df['merge'] = df['project_name'] + df['event_name']
-print(df)
-df.to_csv(resultPath +'mean_detailed_merged.csv')
