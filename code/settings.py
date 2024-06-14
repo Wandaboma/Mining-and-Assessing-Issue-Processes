eccf5eac1cf1eb9bfb231d5ba387ae94121752bc
Wandaboma <42309408+Wandaboma@users.noreply.github.com>
2024-06-14 16:15:20 +0800

    Update settings.py

--- code/settings.py
+++ code/settings.py
@@ -1,22 +1,3 @@
-path = "/home/sbh/APSEC/"
-
-repos = ['angular/angular', 'tensorflow/tensorflow', 'microsoft/vscode']
-
-repos = ['microsoft/vscode', 'flutter/flutter', 'golang/go', 'dart-lang/sdk','dotnet/runtime', 'elastic/kibana', 'rust-lang/rust', 'kubernetes/kubernetes', 'cockroachdb/cockroach', 'tensorflow/tensorflow','microsoft/TypeScript', 'godotengine/godot', 'ansible/ansible', 'elastic/elasticsearch', 'rancher/rancher', 'dotnet/roslyn', 'home-assistant/core', 'qgis/QGIS', 'dotnet/aspnetcore', 'pytorch/pytorch']
-
+path = "/home/"
 repos = ['microsoft/vscode', 'flutter/flutter', 'golang/go','dotnet/runtime', 'elastic/kibana', 'rust-lang/rust', 'kubernetes/kubernetes', 'cockroachdb/cockroach', 'tensorflow/tensorflow','microsoft/TypeScript', 'godotengine/godot', 'ansible/ansible', 'elastic/elasticsearch', 'rancher/rancher', 'dotnet/roslyn', 'home-assistant/core', 'dotnet/aspnetcore', 'pytorch/pytorch']
-
-# repos = ['microsoft/vscode', 'flutter/flutter', 'golang/go', 'dart-lang/sdk', 'dotnet/runtime', 'elastic/kibana', 'rust-lang/rust', 'cockroachdb/cockroach', 'tensorflow/tensorflow','microsoft/TypeScript', 'godotengine/godot', 'rancher/rancher', 'dotnet/roslyn', 'home-assistant/core', 'qgis/QGIS', 'dotnet/aspnetcore', 'pytorch/pytorch']
-
-# repos = ['tensorflow/tensorflow']
-# repos = ['dart-lang/sdk', 'qgis/QGIS']
-
 issueTypes = ['all']
-
-windowLength = 3
-stepLength = 3
-
-TOP_K = 4
-STOP_THRESHOLD = 0.8
-
-alpha = 0.3
